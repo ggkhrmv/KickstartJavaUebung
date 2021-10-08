edfa8cb63583be4539220ba5a9834ee630fe3269
ggkhrmv <gg.khrmv@gmail.com>
2021-10-08 10:31:46 +0200

    Uebung 2 fertig

---
+++ KickstartJava/src/ggkhrmv/kickstart/Uebung2.java
@@ -0,0 +1,25 @@
+package ggkhrmv.kickstart;
+
+public class Uebung2 {
+
+    static double celsius(double fahrenheit) {
+        return ((5.0 / 9.0) * (fahrenheit - 32));
+    }
+
+    public static void AusgabeTabelle() {
+        System.out.println("Fahrenheit\tCelsius");
+        System.out.println("====================");
+
+        for (int x = 0; x <= 100; x++) {
+            System.out.print(x);
+            System.out.print("\t\t");
+            System.out.print(celsius(x));
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        AusgabeTabelle();
+    }
+
+}
