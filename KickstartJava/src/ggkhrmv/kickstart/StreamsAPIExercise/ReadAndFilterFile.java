c5b5efd1ae62719ae25ab73b1cd815f852785490
ggkhrmv <gg.khrmv@gmail.com>
2021-10-14 10:06:57 +0200

    StreamsAPITest added replaceAll for Whitespaces at start of new line

--- KickstartJava/src/ggkhrmv/kickstart/StreamsAPIExercise/ReadAndFilterFile.java
+++ KickstartJava/src/ggkhrmv/kickstart/StreamsAPIExercise/ReadAndFilterFile.java
@@ -6,7 +6,6 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ReadAndFilterFile {
 
