93e97dce29285a7ce2d68dbc99aebcef8db055a3
ggkhrmv <gg.khrmv@gmail.com>
2021-11-09 11:44:47 +0100

    Testing database done

---
+++ KickstartJava/src/ggkhrmv/kickstart/DatabaseTest/Creds.java
@@ -0,0 +1,9 @@
+package ggkhrmv.kickstart.DatabaseTest;
+
+public class Creds {
+
+    public static String url="jdbc:mysql://mystart.co0et8e2gcfz.eu-central-1.rds.amazonaws.com:3306";
+    public static String userName ="admin";
+    public static String pw = "MyStart2021";
+
+}
